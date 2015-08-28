70a6117afc259df62d8ea5514ebf006095fb2560
shiva <shiva0403@gmail.com>
2015-08-28 12:46:17 -0500

    First Checkin

--- Simple/src/org/git/Main.java
+++ Simple/src/org/git/Main.java
@@ -8,7 +8,13 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		display();
+		
+	}
 
+	private static void display() {
+		// TODO Auto-generated method stub
+		System.out.println("This is test");
 	}
 
 }
